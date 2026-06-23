a5830fc4200190ddb4a6aa572e3c34e89adda73d
KyleKW1 <113841695+KyleKW1@users.noreply.github.com>
2026-06-22 23:53:49 -0500

    Make username query case-insensitive

--- database.py
+++ database.py
@@ -288,7 +288,7 @@ def get_user_by_username(username: str):
         return None
     try:
         cur = conn.cursor(dictionary=True)
-        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        cur.execute("SELECT * FROM users WHERE LOWER(username) = LOWER(%s)", (username,))
         user = cur.fetchone()
         cur.close()
         return user
