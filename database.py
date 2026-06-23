f210429494356af68703602ff3de24303f8bc35c
KyleKW1 <113841695+KyleKW1@users.noreply.github.com>
2026-06-23 00:05:18 -0500

    Update database.py

--- database.py
+++ database.py
@@ -296,7 +296,7 @@ def get_user_by_username(username: str):
         st.error(f"Error fetching user: {e}")
         return None
     finally:
-        conn.close()
+        conn.close() 
 
 
 def get_user_by_email(email: str):
